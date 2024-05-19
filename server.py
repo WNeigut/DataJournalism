cdf438914e742cb5c54b01d2651b747333eb6ebb
WNeigut <wyatt.neigut24@trinityschoolnyc.org>
2024-05-19 02:11:42 -0400

    almost final changes

--- server.py
+++ server.py
@@ -1,24 +1,30 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, jsonify, request
 import json
-import math
 
 app = Flask(__name__)
 
+
+with open('./Data/data.json') as f:
+    data = json.load(f)
+
 @app.route('/')
 def index():
-    f = open("Data/data.json","r")
-    data = json.load(f)
-    f.close()
-    
-    return render_template('index.html')
+    latest_year = '2022'
+    total_increase = sum(float(item[latest_year]) for item in data if 'Consumer Price Index item' in item)
+    return render_template('index.html', total_increase=total_increase, latest_year=latest_year)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    return jsonify(data)
+@app.route('/micro')
+def micro():
+    food_type = request.args.get('foodType', 'All food')
+    filtered_data = [item for item in data if item['Consumer Price Index item'] == food_type]
+    if filtered_data:
+        return render_template('micro_page.html', data=filtered_data[0])
+    else:
+        return render_template('micro_page.html', data={})
 
 if __name__ == '__main__':
     app.run(debug=True)
